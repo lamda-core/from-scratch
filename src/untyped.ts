894e39b84c9f78e030a5902d588879a4649a5394
David Cavazos <dcavazos@google.com>
2021-09-24 10:17:09 -0700

    partially fix ackermann

--- src/untyped.ts
+++ src/untyped.ts
@@ -38,7 +38,7 @@ const evaluate = (expr: Expr, env: Env): Expr => {
     switch (expr.kind) {
         case 'Num': return Num(expr.k)
         case 'Var': {
-            let ex = env[expr.x]
+            const ex = env[expr.x]
             if (ex === undefined)
                 return Err(`Undefined variable: ${expr.x}`)
             if (equals(ex, Var(expr.x)))
@@ -47,34 +47,55 @@ const evaluate = (expr: Expr, env: Env): Expr => {
         }
         case 'Let': return evaluate(expr.e, { ...env, ...expr.vars })
         case 'Lam': {
-            let e = evaluate(expr.e, { ...env, [expr.x]: Var(expr.x) })
+            const e = evaluate(expr.e, { ...env, [expr.x]: Var(expr.x) })
             if (e.kind === 'Err') return Err(e.err)
             return Lam(expr.x, e)
         }
         case 'App': {
-            let e1 = evaluate(expr.e1, env)
-            switch (e1.kind) {
-                case 'Num': return Err(`Not a function: ${show(Num(e1.k))}`)
-                case 'Lam': return evaluate(e1.e, { ...env, [e1.x]: Let(env, expr.e2) })
-                case 'Err': return Err(e1.err)
-            }
-            let e2 = evaluate(expr.e2, env)
-            if (e2.kind === 'Err') return Err(e2.err)
-            switch (e1.kind) {
-                case 'Var': return App(Var(e1.x), e2)
-                case 'App':
-                    if (e1.e1.kind === 'Add' && e1.e2.kind === 'Num' && e2.kind === 'Num') return Num(e1.e2.k + e2.k)
-                    if (e1.e1.kind === 'Sub' && e1.e2.kind === 'Num' && e2.kind === 'Num') return Num(e1.e2.k - e2.k)
-                    if (e1.e1.kind === 'Mul' && e1.e2.kind === 'Num' && e2.kind === 'Num') return Num(e1.e2.k * e2.k)
-                    if (e1.e1.kind === 'Eq' && e1.e2.kind === 'Num' && e2.kind === 'Num') {
-                        if (e1.e2.k == e2.k)
-                            return Lam("True", Lam("False", Var("True")))
-                        return Lam("True", Lam("False", Var("False")))
+            switch (expr.e1.kind) {
+                case 'Num': return Err(`Not a function: ${show(Num(expr.e1.k))}`)
+                case 'Var': {
+                    const e1 = evaluate(expr.e1, env)
+                    if (e1.kind === 'Err') return Err(e1.err)
+                    const e2 = evaluate(expr.e2, env)
+                    if (e2.kind === 'Err') return Err(e2.err)
+                    if (e1.kind === 'Lam') {
+                        if (e2.kind === 'Var' && e1.x == e2.x)
+                            return App(Var(expr.e1.x), e2)
+                        return evaluate(App(e1, e2), env)
                     }
+                    return App(e1, e2)
+                }
+                case 'Lam': return evaluate(expr.e1.e, { ...env, [expr.e1.x]: Let(env, expr.e2) })
+                case 'App': {
+                    const e1 = evaluate(expr.e1, env)
+                    if (e1.kind === 'Err') return Err(e1.err)
+                    if (e1.kind === 'App') {
+                        const e2 = evaluate(expr.e2, env)
+                        if (e2.kind === 'Err') return Err(e2.err)
+                        if (e1.e1.kind === 'Add' && e1.e2.kind == 'Num' && e2.kind === 'Num') return Num(e1.e2.k + e2.k)
+                        if (e1.e1.kind === 'Sub' && e1.e2.kind == 'Num' && e2.kind === 'Num') return Num(e1.e2.k - e2.k)
+                        if (e1.e1.kind === 'Mul' && e1.e2.kind == 'Num' && e2.kind === 'Num') return Num(e1.e2.k * e2.k)
+                        if (e1.e1.kind === 'Eq' && e1.e2.kind == 'Num' && e2.kind === 'Num') {
+                            if (e1.e2.k === e2.k)
+                                return Lam("True", Lam("False", Var("True")))
+                            return Lam("True", Lam("False", Var("False")))
+                        }
+                        return App(e1, e2)
+                    }
+                    return evaluate(App(e1, expr.e2), env)
+                }
+                case 'Add': case 'Sub': case 'Mul': case 'Eq': {
+                    const e2 = evaluate(expr.e2, env)
+                    if (e2.kind === 'Err') return Err(e2.err)
+                    return App(expr.e1, e2)
+                }
+                case 'Err': return Err(expr.e1.err)
             }
-            return App(e1, e2)
+
         }
-        case 'Add': case 'Sub': case 'Mul': case 'Eq': case 'Err': return expr
+        case 'Add': case 'Sub': case 'Mul': case 'Eq': return expr
+        case 'Err': return expr
     }
     throw `⚠️ evaluate: not implemented: ${JSON.stringify(expr)}`
 }
@@ -101,15 +122,14 @@ const show = (expr: Expr): string => {
     switch (expr.kind) {
         case 'Num': return `${expr.k}`
         case 'Var': return expr.x
-        case 'Let':
-            if (Object.keys(expr.vars).length == 0)
-                return show(expr.e)
-            return `${showEnv(expr.vars)}; ${show(expr.e)}`
+        case 'Let': return `${showEnv(expr.vars)} ${show(expr.e)}`
         case 'Lam': return `λ${expr.x}. ${show(expr.e)}`
         case 'App':
             if (expr.e1.kind === 'Var' && expr.e2.kind === 'Lam')
                 return `${show(expr.e1)} (${show(expr.e2)})`
             if (expr.e1.kind === 'Var' && expr.e2.kind === 'App')
+                return `${show(expr.e1)} (${show(expr.e2)})`
+            if (expr.e1.kind === 'Var' && expr.e2.kind === 'Let')
                 return `${show(expr.e1)} (${show(expr.e2)})`
             if (expr.e1.kind === 'Lam' && expr.e2.kind === 'Num')
                 return `(${show(expr.e1)}) ${show(expr.e2)}`
@@ -119,31 +139,36 @@ const show = (expr: Expr): string => {
                 return `(${show(expr.e1)}) (${show(expr.e2)})`
             if (expr.e1.kind === 'Lam' && expr.e2.kind === 'App')
                 return `(${show(expr.e1)}) (${show(expr.e2)})`
+            if (expr.e1.kind === 'Lam' && expr.e2.kind === 'Let')
+                return `(${show(expr.e1)}) (${show(expr.e2)})`
             if (expr.e1.kind === 'App' && expr.e2.kind === 'Lam')
                 return `${show(expr.e1)} (${show(expr.e2)})`
             if (expr.e1.kind === 'App' && expr.e2.kind === 'App')
                 return `${show(expr.e1)} (${show(expr.e2)})`
+            if (expr.e1.kind === 'App' && expr.e2.kind === 'Let')
+                return `${show(expr.e1)} (${show(expr.e2)})`
             return `${show(expr.e1)} ${show(expr.e2)}`
-        case 'Add': return '+'
-        case 'Sub': return '-'
-        case 'Mul': return '*'
-        case 'Eq': return '=='
+        case 'Add': return '(+)'
+        case 'Sub': return '(-)'
+        case 'Mul': return '(*)'
+        case 'Eq': return '(==)'
         case 'Err': return expr.err
     }
     throw `⚠️ show: not implemented: ${JSON.stringify(expr)}`
 }
 
 const showEnv = (env: Env): string =>
-    Object.entries(env)
-        .map(([x, expr], _) => `${x} = ${show(expr)}`)
-        .join('; ')
+    '{' + Object.entries(env)
+        .map(([x, expr], _) => `${x}: ${show(expr)}`)
+        .join(', ')
+    + '}'
 
 const check = (e: Expr, env: Env, expected: Expr) => {
-    let got = evaluate(e, env)
+    const got = evaluate(e, env)
     if (equals(got, expected))
-        console.log(`✅ ${show(e)}  ⊣  Γ[${showEnv(env)}]  ∴  ${show(got)}`)
+        console.log(`✅ ${show(e)}  ⊣  Γ${showEnv(env)}  ∴  ${show(got)}`)
     else
-        console.error(`❌ ${show(e)} {${showEnv(env)}} -- got '${show(got)}'; expected '${show(expected)}'`)
+        console.error(`❌ ${show(e)}  ⊣  Γ${showEnv(env)}  --  got '${show(got)}'; expected '${show(expected)}'`)
 }
 
 //-------------------------------------------
@@ -203,10 +228,58 @@ check(Var("f"), { f: Lam("x", App(Var("f"), (Var("x")))) }, Lam("x", App(Var("f"
 check(Var("f"), { f: Lam("x", App(Var("x"), (Var("f")))) }, Lam("x", App(Var("x"), Var("f"))))
 
 console.log('\n☯︎ Factorial')
-// f 0 = 1
-// f n = n * f (n - 1)
-const factorial = Lam("n", app(eq(Var("n"), Num(0)), [Num(1), mul(Var("n"), App(Var("f"), sub(Var("n"), Num(1))))]))
-check(Var("f"), { f: factorial }, factorial)
-check(App(Var("f"), Num(0)), { f: factorial }, Num(1))
-check(App(Var("f"), Num(1)), { f: factorial }, Num(1))
-check(App(Var("f"), Num(5)), { f: factorial }, Num(120))
+{
+    // f 0 = 1
+    // f n = n * f (n - 1)
+    const f = Var("f")
+    const n = Var("n")
+    const k0 = Num(0)
+    const k1 = Num(1)
+
+    const factorial =
+        Lam("n",
+            app(eq(n, k0), [
+                k1,
+                mul(n, App(f, sub(n, k1)))
+            ])
+        )
+
+    check(Var("f"), { f: factorial }, factorial)
+    check(App(Var("f"), Var("n")), { f: factorial, n: Var("n") }, App(Var("f"), Var("n")))
+    check(App(Var("f"), Var("n")), { f: factorial, n: Num(0) }, Num(1))
+    check(App(Var("f"), Var("n")), { f: factorial, n: Num(1) }, Num(1))
+    check(App(Var("f"), Var("n")), { f: factorial, n: Num(5) }, Num(120))
+}
+
+
+console.log('\n☯︎ Ackermann function')
+{
+    // a 0 n = n + 1
+    // a m 0 = a (m-1) 1
+    // a m n = a (m-1) (a m (n-1))
+
+    // a 0 = \n. n + 1
+    // a 1 = \n. (\n. n + 1) (a 1 (n-1))
+    // a 1 = \n. a 1 (n-1) + 1
+    const a = Var("a")
+    const m = Var("m")
+    const n = Var("n")
+    const k0 = Num(0)
+    const k1 = Num(1)
+
+    const ackermann =
+        Lam("m", Lam("n",
+            app(eq(m, k0), [
+                add(n, k1),
+                app(eq(n, k0), [
+                    app(a, [sub(m, k1), k1]),
+                    app(a, [sub(m, k1), app(a, [m, sub(n, k1)])]),
+                ])
+            ])
+        ))
+
+    check(Var("a"), { a: ackermann }, ackermann)
+    check(app(Var("a"), [Var("m")]), { a: ackermann, m: Var("m") }, App(Var("a"), Var("m")))
+    check(app(Var("a"), [Var("m")]), { a: ackermann, m: Num(0) }, Lam("n", add(Var("n"), Num(1))))
+    check(app(Var("a"), [Var("m")]), { a: ackermann, m: Num(1) }, Num(0))
+}
