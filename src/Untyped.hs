8d4a995f9f5cd0cb5538ddb2a47a17bb88d4e8ff
David Cavazos <dcavazos@google.com>
2021-09-30 16:54:15 -0700

    simple recursive variable

--- src/Untyped.hs
+++ src/Untyped.hs
@@ -1,9 +1,11 @@
 module Untyped where
 
+import System.Posix.Internals (c_dup2)
+
 data Expr
   = Num Float
   | Var String
-  | Let [(String, Expr)] Expr
+  | Rec String Expr
   | Lam String Expr
   | App Expr Expr
   | Add
@@ -17,8 +19,10 @@ data Error
   | UndefinedVar String
   deriving (Show, Eq)
 
-lam :: [String] -> Expr -> Expr
-lam xs e = foldr Lam e xs
+data Result err a
+  = Ok a
+  | Err err
+  deriving (Show, Eq)
 
 app :: Expr -> [Expr] -> Expr
 app = foldl App
@@ -40,40 +44,129 @@ get x [] = Nothing
 get x ((x', ex) : env) | x == x' = Just ex
 get x (_ : env) = get x env
 
-eval :: Expr -> [(String, Expr)] -> Either Error Expr
-eval (Num k) _ = Right (Num k)
+set :: String -> Expr -> [(String, Expr)] -> [(String, Expr)]
+set x ex [] = [(x, ex)]
+set x ex ((x', _) : env) | x == x' = (x, ex) : env
+set x ex ((y, ey) : env) = (y, ey) : set x ex env
+
+remove :: String -> [(String, Expr)] -> [(String, Expr)]
+remove _ [] = []
+remove x ((x', ex) : env) | x == x' = remove x env
+remove x ((y, ey) : env) = (y, ey) : remove x env
+
+merge :: [(String, Expr)] -> [(String, Expr)] -> [(String, Expr)]
+merge [] env2 = env2
+merge ((x, ex) : env1) env2 = case get x env2 of
+  Just _ -> merge env1 env2
+  Nothing -> (x, ex) : merge env1 env2
+
+eval :: Expr -> [(String, Expr)] -> Result Error Expr
+eval (Num k) _ = Ok (Num k)
 eval (Var x) env = case get x env of
-  Just (Var x') | x == x' -> Right (Var x)
-  Just ex -> eval ex ((x, Var x) : env)
-  Nothing -> Left (UndefinedVar x)
-eval (Let vars e) env = eval e vars
-eval (Lam x e) env = case eval e ((x, Var x) : env) of
-  Right e' -> Right (Lam x e')
-  Left err -> Left err
-eval (App e1 e2) env = case eval e1 env of
-  Right (Num k) -> Left (NotAFunction (Num k))
-  Right (Var x) -> Right (App (Var x) e2)
-  Right (Lam x e) -> eval e ((x, Let env e2) : env)
-  Right (App Add e1') -> case (e1', eval e2 env) of
-    (Num k1, Right (Num k2)) -> Right (Num (k1 + k2))
-    (_, Right e2') -> Right (App (App Add e1') e2')
-    (_, Left err) -> Left err
-  Right (App Sub e1') -> case (e1', eval e2 env) of
-    (Num k1, Right (Num k2)) -> Right (Num (k1 - k2))
-    (_, Right e2') -> Right (App (App Sub e1') e2')
-    (_, Left err) -> Left err
-  Right (App Mul e1') -> case (e1', eval e2 env) of
-    (Num k1, Right (Num k2)) -> Right (Num (k1 * k2))
-    (_, Right e2') -> Right (App (App Mul e1') e2')
-    (_, Left err) -> Left err
-  Right (App Eq e1') -> case (e1', eval e2 env) of
-    (Num k1, Right (Num k2)) | k1 == k2 -> Right (Lam "True" (Lam "False" (Var "True")))
-    (Num k1, Right (Num k2)) -> Right (Lam "True" (Lam "False" (Var "False")))
-    (_, Right e2') -> Right (App (App Eq e1') e2')
-    (_, Left err) -> Left err
-  Right (App a b) -> Right (App (App a b) e2)
-  Right op -> case eval e2 env of
-    Right e2' -> Right (App op e2')
-    Left err -> Left err
-  Left err -> Left err
-eval op _ = Right op
+  Just (Var x') | x == x' -> Ok (Var x)
+  -- Just (Lam y e) -> case eval (Lam y e) (set x (Var x) env) of
+  --   Ok (Rec env' ex) -> Ok (Rec (set x ex env') (Var x))
+  --   Ok ex -> Ok ex
+  --   Err err -> Err err
+  Just (Lam y e) -> eval (Lam y e) (set x (Rec x (Var x)) env)
+  Just e -> eval e (set x (Rec x e) env)
+  Nothing -> Err (UndefinedVar x)
+-- eval (Rec vars e) env = eval e vars
+eval (Rec x (Var x')) _ | x == x' = Ok (Rec x (Var x))
+eval (Rec x e) env = eval e env
+eval (Lam x e) env = case eval e (set x (Var x) env) of
+  Ok (Rec y e') -> Ok (Rec y (Lam x e'))
+  Ok e' -> Ok (Lam x e')
+  Err err -> Err err
+-- eval (Lam x e) env = case eval e (set x (Var x) env) of
+--   Ok (Rec vars e') -> case remove x vars of
+--     [] -> Ok (Lam x e')
+--     vars' -> Ok (Rec vars' (Lam x e'))
+--   Ok e' -> Ok (Lam x e')
+--   Err err -> Err err
+eval (App (Num k) _) _ = Err (NotAFunction (Num k))
+eval (App (Var x) e2) env = case get x env of
+  Just (Var x') | x == x' -> case closure e2 env of
+    -- Ok (Rec env' e2') -> Ok (Rec ((x, Var x) : env') (App (Var x) e2'))
+    Ok e2' -> Ok (App (Var x) e2')
+    Err err -> Err err
+  Just e1' -> eval (App e1' e2) env
+  Nothing -> Err (UndefinedVar x)
+eval (App (Rec x e1) e2) env = case eval (Rec x e1) env of
+  Ok (Rec y e1') -> Ok (Rec y (App e1' e2))
+  Ok e1' -> eval (App e1' e2) env
+  Err err -> Err err
+eval (App (Lam x e) ex) env = case closure (Var x) (set x (Rec x ex) env) of
+  Ok ex' -> eval e (set x ex' env)
+  Err err -> Err err
+eval (App (App Add e1) e2) env = case (eval e1 env, eval e2 env) of
+  (Ok (Num k1), Ok (Num k2)) -> Ok (Num (k1 + k2))
+  (Ok e1', Ok e2') -> Ok (add e1' e2')
+  (Err err, _) -> Err err
+  (_, Err err) -> Err err
+eval (App (App Sub e1) e2) env = case (eval e1 env, eval e2 env) of
+  (Ok (Num k1), Ok (Num k2)) -> Ok (Num (k1 - k2))
+  (Ok e1', Ok e2') -> Ok (sub e1' e2')
+  (Err err, _) -> Err err
+  (_, Err err) -> Err err
+eval (App (App Mul e1) e2) env = case (eval e1 env, eval e2 env) of
+  (Ok (Num k1), Ok (Num k2)) -> Ok (Num (k1 * k2))
+  (Ok e1', Ok e2') -> Ok (mul e1' e2')
+  (Err err, _) -> Err err
+  (_, Err err) -> Err err
+eval (App (App Eq e1) e2) env = case (eval e1 env, eval e2 env) of
+  (Ok (Num k1), Ok (Num k2)) | k1 == k2 -> Ok (Lam "True" (Lam "False" (Var "True")))
+  (Ok (Num k1), Ok (Num k2)) -> Ok (Lam "True" (Lam "False" (Var "False")))
+  (Ok e1', Ok e2') -> Ok (eq e1' e2')
+  (Err err, _) -> Err err
+  (_, Err err) -> Err err
+eval (App (App a b) e2) env = case eval (App a b) env of
+  Ok e1' -> eval (App e1' e2) env
+  Err err -> Err err
+eval (App op e) env = case eval e env of
+  Ok e' -> Ok (App op e')
+  Err err -> Err err
+-- eval (App e1 e2) env = case (eval e1 env, eval e2 env) of
+--   (Ok (Num k), _) -> Err (NotAFunction (Num k))
+--   (Ok (Var x), Ok e2') -> Ok (App (Var x) e2')
+--   (Ok (Rec env1 (Var x)), Ok (Rec env2 e2')) -> Ok (Rec (merge env1 env2) (App (Var x) e2'))
+--   (Ok (Rec env1 e1'), Ok (Rec env2 e2')) -> Ok (Rec (merge env1 env2) (App e1' e2'))
+--   (Ok (Rec env' (Var x)), Ok e2') -> case get x env' of
+--     Just (Var x') | x == x' -> Ok (Rec env' (App (Var x) e2'))
+--     -- Just (Lam y (App a b)) -> case (eval a (set y e2' env'), eval b (set x (Var x) (set y e2' env'))) of
+--     --   (Ok a', Ok b') -> eval (App a' b') env'
+--     --   (Err err, _) -> Err err
+--     --   (_, Err err) -> Err err
+--     Just (Lam y (App a b)) -> case eval a (set y e2' env') of
+--       Ok (Lam z e) -> eval (Rec [(z, Rec (set y e2' env') b)] e) (set y e2' env')
+--       --Ok (Rec (set y e2' env') b)
+--       Err err -> Err err
+--     -- Just e1' -> eval (App e1' e2') (set x (Var x) env')
+--     Just (Lam y (Lam z e)) -> Ok (Rec env' (App (Var x) e2'))
+--     Nothing -> Err (UndefinedVar x)
+--   -- (Ok (Rec env' (App a b)), Ok e2') -> case eval a env' of
+--   --   TODO
+--   --   Err err -> Err err
+--   (Ok (Rec env' e1'), Ok e2') -> Ok (Rec env' (App e1' e2'))
+--   (Ok e1', Ok (Rec env' e2')) -> Ok (Rec env' (App e1' e2'))
+--   (Ok (Lam x e), Ok e2') -> eval e (set x e2' env)
+--   (Ok (App Add (Num k1)), Ok (Num k2)) -> Ok (Num (k1 + k2))
+--   (Ok (App Sub (Num k1)), Ok (Num k2)) -> Ok (Num (k1 - k2))
+--   (Ok (App Mul (Num k1)), Ok (Num k2)) -> Ok (Num (k1 * k2))
+--   (Ok (App Eq (Num k1)), Ok (Num k2)) | k1 == k2 -> Ok (Lam "True" (Lam "False" (Var "True")))
+--   (Ok (App Eq (Num k1)), Ok (Num k2)) -> Ok (Lam "True" (Lam "False" (Var "False")))
+--   (Ok e1', Ok e2') -> Ok (App e1' e2')
+--   (Err err, _) -> Err err
+--   (_, Err err) -> Err err
+eval Add _ = Ok Add
+eval Sub _ = Ok Sub
+eval Mul _ = Ok Mul
+eval Eq _ = Ok Eq
+
+-- eval e _ = Err (NotAFunction e)
+
+closure :: Expr -> [(String, Expr)] -> Result Error Expr
+closure e@(Num _) env = eval e env
+closure e@(Var x) env = eval e env
+closure e@(App _ _) env = eval e env
+closure e _ = Err (NotAFunction e)
--- test/UntypedTest.hs
+++ test/UntypedTest.hs
@@ -3,119 +3,161 @@ import Untyped
 
 main :: IO ()
 main = hspec $ do
+  describe "☯︎ Merge" $ do
+    it "{x: 1} {x: 1}  ∴  {x: 1}" $ do
+      merge [("x", Num 1)] [("x", Num 1)] `shouldBe` [("x", Num 1)]
+    it "{x: 1} {x: 2}  ∴  {x: 1}" $ do
+      merge [("x", Num 1)] [("x", Num 2)] `shouldBe` [("x", Num 2)]
+    it "{x: 1} {y: 2}  ∴  {x: 1, y: 2}" $ do
+      merge [("x", Num 1)] [("y", Num 2)] `shouldBe` [("x", Num 1), ("y", Num 2)]
+    it "{x: 1, y: 2} {z: 3, w: 4}  ∴  {x: 1, y: 2}" $ do
+      merge [("x", Num 1), ("y", Num 2)] [("z", Num 3), ("w", Num 4)] `shouldBe` [("x", Num 1), ("y", Num 2), ("z", Num 3), ("w", Num 4)]
+
   describe "☯︎ Number" $ do
     it "1  ∴  1" $ do
-      eval (Num 1) [] `shouldBe` Right (Num 1)
+      eval (Num 1) [] `shouldBe` Ok (Num 1)
 
   describe "☯︎ Variable" $ do
     it "x  ∴  Undefined variable: x" $ do
-      eval (Var "x") [] `shouldBe` Left (UndefinedVar "x")
+      eval (Var "x") [] `shouldBe` Err (UndefinedVar "x")
+    it "x  Γ{x: 1}  ∴  1" $ do
+      eval (Var "x") [("x", Num 1)] `shouldBe` Ok (Num 1)
     it "x  Γ{x: x}  ∴  x" $ do
-      eval (Var "x") [("x", Var "x")] `shouldBe` Right (Var "x")
+      eval (Var "x") [("x", Var "x")] `shouldBe` Ok (Var "x")
     it "x  Γ{y: 1, x: y}  ∴  1" $ do
-      eval (Var "x") [("y", Num 1), ("x", Var "y")] `shouldBe` Right (Num 1)
+      eval (Var "x") [("y", Num 1), ("x", Var "y")] `shouldBe` Ok (Num 1)
+  -- it "x  Γ{x: λy. y}  ∴  λy. y" $ do
+  --   eval (Var "x") [("x", Lam "y" (Var "y"))] `shouldBe` Ok (Lam "y" (Var "y"))
+  -- it "x  Γ{x: f = f}  ∴  f = f" $ do
+  --   eval (Var "x") [("x", Rec "f" (Var "f"))] `shouldBe` Ok (Rec "f" (Var "f"))
 
-  -- describe "☯︎ Let bindings" $ do
-  --   it "{} x  Γ{x: 1}  ∴  1" $ do
-  --     eval (Let [] (Var "x")) [("x", Num 1)] `shouldBe` Right (Num 1)
-  --   it "{x: 1} x  Γ{x: 2}  ∴  1" $ do
-  --     eval (Let [("x", Num 1)] (Var "x")) [("x", Num 2)] `shouldBe` Right (Num 1)
+  -- describe "☯︎ Rec bindings" $ do
+  --   it "{} 1  ∴  1" $ do
+  --     eval (Rec [] (Num 1)) [] `shouldBe` Ok (Num 1) []
+  --   it "{} x  ∴  Undefined variable: x" $ do
+  --     eval (Rec [] (Var "x")) [] `shouldBe` Err (UndefinedVar "x")
+  --   it "{} ({} 1)  ∴  1" $ do
+  --     eval (Rec [] (Rec [] (Num 1))) [] `shouldBe` Ok (Num 1) []
+  --   it "{} (λx. 1)  ∴  λx. 1" $ do
+  --     eval (Rec [] (Lam "x" (Num 1))) [] `shouldBe` Ok (Lam "x" (Num 1)) []
 
-  describe "☯︎ Lamda abstraction" $ do
+  describe "☯︎ Recursive definition" $ do
+    it "x = x  ∴  x = x" $ do
+      eval (Rec "x" (Var "x")) [] `shouldBe` Ok (Rec "x" (Var "x"))
+    it "x = 1  ∴  1" $ do
+      eval (Rec "f" (Num 1)) [] `shouldBe` Ok (Num 1)
+
+  describe "☯︎ λ Abstraction" $ do
     it "λx. y  ∴  Undefined variable: y" $ do
-      eval (Lam "x" (Var "y")) [] `shouldBe` Left (UndefinedVar "y")
+      eval (Lam "x" (Var "y")) [] `shouldBe` Err (UndefinedVar "y")
+    it "λx. 1  ∴  λx. 1" $ do
+      eval (Lam "x" (Num 1)) [] `shouldBe` Ok (Lam "x" (Num 1))
     it "λx. x  ∴  λx. x" $ do
-      eval (Lam "x" (Var "x")) [] `shouldBe` Right (Lam "x" (Var "x"))
+      eval (Lam "x" (Var "x")) [] `shouldBe` Ok (Lam "x" (Var "x"))
+    it "λx. y  Γ{y: y}  ∴  λx. y" $ do
+      eval (Lam "x" (Var "y")) [("y", Var "y")] `shouldBe` Ok (Lam "x" (Var "y"))
 
   describe "☯︎ Application" $ do
     it "1 2  ∴  Not a function" $ do
-      eval (App (Num 1) (Num 2)) [] `shouldBe` Left (NotAFunction (Num 1))
-
+      eval (App (Num 1) (Num 2)) [] `shouldBe` Err (NotAFunction (Num 1))
     it "f x  ∴  Undefined variable: f" $ do
-      eval (App (Var "f") (Var "x")) [] `shouldBe` Left (UndefinedVar "f")
+      eval (App (Var "f") (Var "x")) [] `shouldBe` Err (UndefinedVar "f")
     it "f x  Γ{f: f}  ∴  Undefined variable: x" $ do
-      eval (App (Var "f") (Var "x")) [("f", Var "f")] `shouldBe` Right (App (Var "f") (Var "x"))
-    it "f x  Γ{f: λy. 1, x: x}  ∴  1" $ do
-      eval (App (Var "f") (Var "x")) [("f", Lam "y" (Num 1)), ("x", Var "x")] `shouldBe` Right (Num 1)
-    it "f x  Γ{f: λy. λz. 1, x: 2}  ∴  λz. 1" $ do
-      eval (App (Var "f") (Var "x")) [("f", Lam "y" (Lam "z" (Num 1))), ("x", Num 1)] `shouldBe` Right (Lam "z" (Num 1))
-
-    it "(λx. 1) x  Γ{x: 2}  ∴  1" $ do
-      eval (App (Lam "x" (Num 1)) (Var "x")) [("x", Num 2)] `shouldBe` Right (Num 1)
-
-    it "((λx. x) (λy. y)) x  Γ{x: 1}  ∴  1" $ do
-      eval (App (App (Lam "x" (Var "x")) (Lam "y" (Var "y"))) (Var "x")) [("x", Num 1)] `shouldBe` Right (Num 1)
+      eval (App (Var "f") (Var "x")) [("f", Var "f")] `shouldBe` Err (UndefinedVar "x")
+    it "f 1  Γ{f: f}  ∴  f 1" $ do
+      eval (App (Var "f") (Num 1)) [("f", Var "f")] `shouldBe` Ok (App (Var "f") (Num 1))
+    it "f x  Γ{f: f, x: x}  ∴  f x" $ do
+      eval (App (Var "f") (Var "x")) [("f", Var "f"), ("x", Var "x")] `shouldBe` Ok (App (Var "f") (Var "x"))
+    it "(f = f) 1  ∴  f = f 1" $ do
+      eval (App (Rec "f" (Var "f")) (Num 1)) [] `shouldBe` Ok (Rec "f" (App (Var "f") (Num 1)))
+    it "(λx. 1) 2  ∴  1" $ do
+      eval (App (Lam "x" (Num 1)) (Num 2)) [] `shouldBe` Ok (Num 1)
+    it "(λx. x) 1  ∴  1" $ do
+      eval (App (Lam "x" (Var "x")) (Num 1)) [] `shouldBe` Ok (Num 1)
+    it "f 2  Γ{f: λx. 1}  ∴  1" $ do
+      eval (App (Var "f") (Num 2)) [("f", Lam "x" (Num 1))] `shouldBe` Ok (Num 1)
+    it "((λx. x) (λy. y)) 1  ∴  1" $ do
+      eval (App (App (Lam "x" (Var "x")) (Lam "y" (Var "y"))) (Num 1)) [] `shouldBe` Ok (Num 1)
+  --   it "((λx. x) (λy. x)) 1  ∴  1" $ do
+  --     eval (App (App (Lam "x" (Var "x")) (Lam "y" (Var "x"))) (Num 1)) [] `shouldBe` Ok (Num 1)
 
   describe "☯︎ Addition" $ do
     it "(+)  ∴  (+)" $ do
-      eval Add [] `shouldBe` Right Add
-    it "(+) x  Γ{x: 1}  ∴  (+) 1" $ do
-      eval (App Add (Var "x")) [("x", Num 1)] `shouldBe` Right (App Add (Num 1))
+      eval Add [] `shouldBe` Ok Add
+    it "(+) x  Γ{x: x}  ∴  (+) x" $ do
+      eval (App Add (Var "x")) [("x", Var "x")] `shouldBe` Ok (App Add (Var "x"))
+    it "(+) x y  Γ{x: x, y: y}  ∴  x + y" $ do
+      eval (add (Var "x") (Var "y")) [("x", Var "x"), ("y", Var "y")] `shouldBe` Ok (add (Var "x") (Var "y"))
     it "(+) x y  Γ{x: 1, y: 2}  ∴  3" $ do
-      eval (add (Var "x") (Var "y")) [("x", Num 1), ("y", Num 2)] `shouldBe` Right (Num 3)
+      eval (add (Var "x") (Var "y")) [("x", Num 1), ("y", Num 2)] `shouldBe` Ok (Num 3)
 
   describe "☯︎ Subtraction" $ do
     it "(-)  ∴  (-)" $ do
-      eval Sub [] `shouldBe` Right Sub
-    it "(-) x  Γ{x: 1}  ∴  (-) 1" $ do
-      eval (App Sub (Var "x")) [("x", Num 1)] `shouldBe` Right (App Sub (Num 1))
+      eval Sub [] `shouldBe` Ok Sub
+    it "(-) x  Γ{x: x}  ∴  (-) x" $ do
+      eval (App Sub (Var "x")) [("x", Var "x")] `shouldBe` Ok (App Sub (Var "x"))
+    it "(-) x y  Γ{x: x, y: y}  ∴  x - y  Γ{x: x, y: y}" $ do
+      eval (sub (Var "x") (Var "y")) [("x", Var "x"), ("y", Var "y")] `shouldBe` Ok (sub (Var "x") (Var "y"))
     it "(-) x y  Γ{x: 1, y: 2}  ∴  -1" $ do
-      eval (sub (Var "x") (Var "y")) [("x", Num 1), ("y", Num 2)] `shouldBe` Right (Num (-1))
+      eval (sub (Var "x") (Var "y")) [("x", Num 1), ("y", Num 2)] `shouldBe` Ok (Num (-1))
 
   describe "☯︎ Multiplication" $ do
     it "(*)  ∴  (*)" $ do
-      eval Mul [] `shouldBe` Right Mul
-    it "(*) x  Γ{x: 1}  ∴  (*) 1" $ do
-      eval (App Mul (Var "x")) [("x", Num 1)] `shouldBe` Right (App Mul (Num 1))
+      eval Mul [] `shouldBe` Ok Mul
+    it "(*) x  Γ{x: x}  ∴  (*) x" $ do
+      eval (App Mul (Var "x")) [("x", Var "x")] `shouldBe` Ok (App Mul (Var "x"))
+    it "(*) x y  Γ{x: x, y: y}  ∴  x * y  Γ{x: x, y: y}" $ do
+      eval (mul (Var "x") (Var "y")) [("x", Var "x"), ("y", Var "y")] `shouldBe` Ok (mul (Var "x") (Var "y"))
     it "(*) x y  Γ{x: 1, y: 2}  ∴  2" $ do
-      eval (mul (Var "x") (Var "y")) [("x", Num 1), ("y", Num 2)] `shouldBe` Right (Num 2)
+      eval (mul (Var "x") (Var "y")) [("x", Num 1), ("y", Num 2)] `shouldBe` Ok (Num 2)
 
   describe "☯︎ Equals" $ do
     it "(==)  ∴  (==)" $ do
-      eval Eq [] `shouldBe` Right Eq
-    it "(==) x  Γ{x: 1}  ∴  (==) 1" $ do
-      eval (App Eq (Var "x")) [("x", Num 1)] `shouldBe` Right (App Eq (Num 1))
+      eval Eq [] `shouldBe` Ok Eq
+    it "(==) x  Γ{x: x}  ∴  (==) x" $ do
+      eval (App Eq (Var "x")) [("x", Var "x")] `shouldBe` Ok (App Eq (Var "x"))
+    it "(==) x y  Γ{x: x, y: y}  ∴  x == y  Γ{x: x, y: y}" $ do
+      eval (eq (Var "x") (Var "y")) [("x", Var "x"), ("y", Var "y")] `shouldBe` Ok (eq (Var "x") (Var "y"))
     it "(==) x y  Γ{x: 1, y: 2}  ∴  λTrue False. False" $ do
-      eval (eq (Var "x") (Var "y")) [("x", Num 1), ("y", Num 2)] `shouldBe` Right (Lam "True" (Lam "False" (Var "False")))
+      eval (eq (Var "x") (Var "y")) [("x", Num 1), ("y", Num 2)] `shouldBe` Ok (Lam "True" (Lam "False" (Var "False")))
     it "(==) x y  Γ{x: 2, y: 2}  ∴  λTrue False. False" $ do
-      eval (eq (Var "x") (Var "y")) [("x", Num 2), ("y", Num 2)] `shouldBe` Right (Lam "True" (Lam "False" (Var "True")))
+      eval (eq (Var "x") (Var "y")) [("x", Num 2), ("y", Num 2)] `shouldBe` Ok (Lam "True" (Lam "False" (Var "True")))
     it "(==) x y  Γ{x: 3, y: 2}  ∴  λTrue False. False" $ do
-      eval (eq (Var "x") (Var "y")) [("x", Num 3), ("y", Num 2)] `shouldBe` Right (Lam "True" (Lam "False" (Var "False")))
+      eval (eq (Var "x") (Var "y")) [("x", Num 3), ("y", Num 2)] `shouldBe` Ok (Lam "True" (Lam "False" (Var "False")))
 
-  describe "☯︎ Factorial" $ do
-    it "f  Γ{f: factorial}  ∴  factorial" $ do
-      eval (Var "f") [("f", factorial)] `shouldBe` Right factorial
-    it "f n  Γ{f: factorial, n: n}  ∴  factorial" $ do
-      eval (App (Var "f") (Var "n")) [("f", factorial), ("n", Var "n")] `shouldBe` Right (app (eq n k0) [k1, mul n (App f (sub n k1))])
-    it "f n  Γ{f: factorial, n: 0}  ∴  1" $ do
-      eval (App (Var "f") (Var "n")) [("f", factorial), ("n", Num 0)] `shouldBe` Right (Num 1)
-    it "f n  Γ{f: factorial, n: 1}  ∴  1" $ do
-      eval (App (Var "f") (Var "n")) [("f", factorial), ("n", Num 1)] `shouldBe` Right (Num 1)
-    it "f n  Γ{f: factorial, n: 2}  ∴  2" $ do
-      eval (App (Var "f") (Var "n")) [("f", factorial), ("n", Num 2)] `shouldBe` Right (Num 2)
-    it "f n  Γ{f: factorial, n: 5}  ∴  120" $ do
-      eval (App (Var "f") (Var "n")) [("f", factorial), ("n", Num 5)] `shouldBe` Right (Num 120)
+  describe "☯︎ Simple recursion" $ do
+    it "f  Γ{f: λx. f x}  ∴  f  {f: λx. f x} f" $ do
+      eval (Var "f") [("f", Lam "x" (App (Var "f") (Var "x")))] `shouldBe` Ok (Rec "f" (Lam "x" (App (Var "f") (Var "x"))))
+  --   it "f x  Γ{f: λx. f x, x: x}  ∴  {f: λx. f x, x: x} f x" $ do
+  --     eval (App (Var "f") (Var "x")) [("f", Lam "x" (App (Var "f") (Var "x"))), ("x", Var "x")] `shouldBe` Ok (App (Rec "f" (Lam "x" (App (Var "f") (Var "x")))) (Var "x"))
+  --   it "f x  Γ{f: λx. x f, x: x}  ∴  {f: λx. f x, x: x} f x" $ do
+  --     eval (App (Var "f") (Var "x")) [("f", Lam "x" (App (Var "x") (Var "f"))), ("x", Var "x")] `shouldBe` Ok (Rec [("f", Lam "x" (App (Var "f") (Var "x"))), ("x", Var "x")] (App (Var "f") (Var "x")))
 
-  describe "☯︎ Ackermann" $ do
-    -- https://en.wikipedia.org/wiki/Ackermann_function
-    it "a  Γ{a: ackermann}  ∴  ackermann" $ do
-      eval (Var "a") [("a", ackermann)] `shouldBe` Right ackermann
-    it "a m  Γ{a: ackermann, m: m}  ∴  λn. ackermann" $ do
-      eval (App (Var "a") (Var "m")) [("a", ackermann), ("m", Var "m")] `shouldBe` Right (Lam "n" (app (eq m k0) [add n k1, app (eq n k0) [app a [sub m k1, k1], app a [sub m k1, app a [m, sub n k1]]]]))
-    it "a m  Γ{a: ackermann, m: 0}  ∴  λn. n + 1" $ do
-      eval (App (Var "a") (Var "m")) [("a", ackermann), ("m", Num 0)] `shouldBe` Right (Lam "n" (add n k1))
-    it "a m  Γ{a: ackermann, m: 1}  ∴  a 0" $ do
-      eval (App (Var "a") (Var "m")) [("a", ackermann), ("m", Num 1)] `shouldBe` Right (Lam "n" (app (eq n k0) [app a [sub m k1, k1], app a [sub m k1, app a [m, sub n k1]]]))
-    it "a m  Γ{a: ackermann, m: 3}  ∴  a 0" $ do
-      eval (App (Var "a") (Var "m")) [("a", ackermann), ("m", Num 3)] `shouldBe` Right (Lam "n" (app (eq n k0) [app a [sub m k1, k1], app a [sub m k1, app a [m, sub n k1]]]))
-    it "a m n  Γ{a: ackermann, m: 0, n: 0}  ∴  1" $ do
-      eval (app (Var "a") [Var "m", Var "n"]) [("a", ackermann), ("m", Num 0), ("n", Num 0)] `shouldBe` Right (Num 1)
-    it "a m n  Γ{a: ackermann, m: 1, n: 1}  ∴  3" $ do
-      eval (app (Var "a") [Var "m", Var "n"]) [("a", ackermann), ("m", Num 1), ("n", Num 1)] `shouldBe` Right (Num 3)
-    it "a m n  Γ{a: ackermann, m: 1, n: 3}  ∴  5" $ do
-      eval (app (Var "a") [Var "m", Var "n"]) [("a", ackermann), ("m", Num 1), ("n", Num 3)] `shouldBe` Right (Num 5)
-    it "a m n  Γ{a: ackermann, m: 2, n: 0}  ∴  4" $ do
-      eval (app (Var "a") [Var "m", Var "n"]) [("a", ackermann), ("m", Num 2), ("n", Num 0)] `shouldBe` Right (Num 3)
+  -- describe "☯︎ Factorial" $ do
+  --   it "f  Γ{f: factorial}  ∴  {f: factorial} f" $ do
+  --     eval (Var "f") [("f", factorial)] `shouldBe` Ok (Rec [("f", factorial)] (Var "f"))
+  --   it "f n  Γ{f: factorial, n: n}  ∴  {f: factorial, n: n} f n" $ do
+  --     eval (App (Var "f") (Var "n")) [("f", factorial), ("n", Var "n")] `shouldBe` Ok (Rec [("f", factorial), ("n", Var "n")] (App (Var "f") (Var "n")))
+  --   it "f 0  Γ{f: factorial}  ∴  1" $ do
+  --     eval (App (Var "f") (Num 0)) [("f", factorial)] `shouldBe` Ok (Num 1)
+  --   it "f 1  Γ{f: factorial}  ∴  1" $ do
+  --     eval (App (Var "f") (Num 1)) [("f", factorial)] `shouldBe` Ok (Num 1)
+  --   it "f 2  Γ{f: factorial}  ∴  2" $ do
+  --     eval (App (Var "f") (Num 2)) [("f", factorial)] `shouldBe` Ok (Num 2)
+  --   it "f 5  Γ{f: factorial}  ∴  120" $ do
+  --     eval (App (Var "f") (Num 5)) [("f", factorial)] `shouldBe` Ok (Num 120)
+
+  -- describe "☯︎ Ackermann" $ do
+  --   it "a  Γ{a: ackermann}  ∴  {a: ackermann} a" $ do
+  --     eval (Var "a") [("a", ackermann)] `shouldBe` Ok (Rec [("a", ackermann)] (Var "a"))
+  --   it "a m  Γ{a: ackermann, m: m}  ∴  {a: ackermann, m: m} a m" $ do
+  --     eval (App (Var "a") (Var "m")) [("a", ackermann), ("m", Var "m")] `shouldBe` Ok (Rec [("a", ackermann), ("m", Var "m")] (App (Var "a") (Var "m")))
+  --   it "a 0  Γ{a: ackermann}  ∴  {a: ackermann} a 0" $ do
+  --     eval (App (Var "a") (Num 0)) [("a", ackermann)] `shouldBe` Ok (Rec [("a", ackermann)] (App (Var "a") (Num 0)))
+  --   it "a 0 0  Γ{a: ackermann}  ∴  1" $ do
+  --     eval (app (Var "a") [Num 0, Num 0]) [("a", ackermann)] `shouldBe` Ok (Num 1)
+
+  it "asdf" $ do 1 `shouldBe` 1
   where
     a = Var "a"
     f = Var "f"
@@ -123,30 +165,14 @@ main = hspec $ do
     n = Var "n"
     k0 = Num 0
     k1 = Num 1
+
     -- f 0 = 1
     -- f n = n * f (n - 1)
     factorial =
-      Lam
-        "n"
-        ( app
-            (eq n k0)
-            [ k1,
-              mul n (App f (sub n k1))
-            ]
-        )
+      Lam "n" (app (eq n k0) [k1, mul n (App f (sub n k1))])
+
     -- a 0 n = n + 1
     -- a m 0 = a (m-1) 1
     -- a m n = a (m-1) (a m (n-1))
     ackermann =
-      lam
-        ["m", "n"]
-        ( app
-            (eq m k0)
-            [ add n k1,
-              app
-                (eq n k0)
-                [ app a [sub m k1, k1],
-                  app a [sub m k1, app a [m, sub n k1]]
-                ]
-            ]
-        )
+      Lam "m" (Lam "n" (app (eq m k0) [add n k1, app (eq n k0) [app a [sub m k1, k1], app a [sub m k1, app a [m, sub n k1]]]]))
